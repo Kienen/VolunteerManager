d785f192fb87d8cccde81ba8db16c39ea062a423
Kienen <kienen.mason@gmail.com>
2015-06-19 12:16:28 -0700

    Development Branch First commit
    Reverted to an old settings.py and wsgi.py to continue development in a
    Windows environment.

--- VolunteerManager/settings.py
+++ VolunteerManager/settings.py
@@ -1,17 +1,15 @@
 """
 Django settings for VolunteerManager project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-'''
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-'''
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -97,33 +95,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 LOGIN_REDIRECT_URL = '/home/'
 
 SITE_ID = 1
-
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
