173b3ea407eac8bc734142d2ac02d94e78278672
Kienen <kienen.mason@gmail.com>
2015-06-28 22:06:48 -0700

    .

--- VolunteerManager/settings.py
+++ VolunteerManager/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
